5adc966843db208de23f5ce1e66c72280656947b
mashyycel <95265421+mashyycel@users.noreply.github.com>
2023-05-13 15:23:14 +0700

    nambah route

--- webdev/routes/web.php
+++ webdev/routes/web.php
@@ -14,5 +14,75 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/glass', function () {
+    return view('glass');
+});
+Route::get('/gourmet', function () {
+    return view('gourmet');
+});
+Route::get('/hdpe', function () {
+    return view('hdpe');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/offers', function () {
+    return view('offers');
+});
+
+Route::get('/opp', function () {
+    return view('opp');
+});
+
+Route::get('/paper', function () {
+    return view('paper');
+});
+
+Route::get('/pe', function () {
+    return view('pe');
+});
+
+Route::get('/plastics', function () {
+    return view('plastics');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/registered', function () {
+    return view('registered');
+});
+
+Route::get('/short-codes', function () {
+    return view('short-codes');
+});
+
+Route::get('/single', function () {
+    return view('single');
 });
