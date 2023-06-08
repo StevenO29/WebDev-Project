154a445602d6236a36a17c02e863c588696b07d1
mashyycel <95265421+mashyycel@users.noreply.github.com>
2023-06-08 19:40:51 +0700

    change a bit

--- app/Models/listOrder.php
+++ app/Models/listOrder.php
@@ -15,12 +15,15 @@ class listOrder extends Model
     public function tabelorder(){
         $server = "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));";
         $run = DB::select($server);
-        $value = "SELECT o.order_id AS `ID`,c.cust_name AS `name`,sum(od.o_qty*od.p_price) AS `subtotal`,
-        o.order_status AS `status`,s.Carrier_Name AS `carrier`,s.Tracking_Num AS `tracking`,
+        $value = "SELECT o.order_id AS `ID`, c.cust_name AS `name`, SUM(od.o_qty * od.p_price) AS `subtotal`,
+        o.order_status AS `status`, s.Carrier_Name AS `carrier`, s.Tracking_Num AS `tracking`,
         od.p_name AS `prod_name`, od.p_price AS `price`, od.o_qty AS `qty`
-         FROM order2 o, Customer c, Order_Details od, Shipping s 
-         where o.cust_id = c.cust_id and o.order_id = od.order_id and s.order_id = o.order_id and o.status_del = '0'
-             group by o.order_id;       ";
+ FROM order2 o
+ LEFT JOIN Customer c ON o.cust_id = c.cust_id
+ LEFT JOIN Order_Details od ON o.order_id = od.order_id
+ LEFT JOIN Shipping s ON s.order_id = o.order_id
+ WHERE o.status_del = '0'
+ GROUP BY o.order_id;       ";
         
         $selectorder = DB::select($value);
         return collect($selectorder);
