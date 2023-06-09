91b3a17e34102fd39b539d69cae73e7ed4fadae0
mchristanto <mchristanto01@student.ciputra.ac.id>
2023-06-09 07:59:30 +0700

    Revert "Revert "css""
    This reverts commit a4d8c379545b55725dfed3d0c195cb37a27c234a.

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -341,10 +341,11 @@
                 >
               </li>
               <li>
-                <i class="glyphicon glyphicon-earphone" aria-hidden="true"></i
-                >+62 896-3781-5553
+                <i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>                  
+                <a href="https://wa.me/+6289637815553">+62 896-3781-5553</a>
               </li>
             </ul>
+
           </div>
           <div class="col-md-3 w3_footer_grid">
             <h3>Information</h3>
@@ -425,23 +426,7 @@
         </div>
       </div>
     </div>
-    <div class="footer-botm">
-      <div class="container">
-        <div class="w3layouts-foot">
-          <ul>
-            <li>
-              <a href="wa.me/089637815553" class="w3_agile_whatsapp"
-                ><i class="fa fa-whatsapp" aria-hidden="true"></i
-              ></a>
-            </li>
-          </ul>
-        </div>
-        <div class="payment-w3ls">
-          <img src="images/card.png" alt=" " class="img-responsive" />
-        </div>
-        <div class="clearfix"></div>
-      </div>
-    </div>
+ 
     <!-- //footer -->
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
